c1b2dfdd47d28b0969751b3a800c2d38982deecb
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-08-25 14:06:53 -0500

    Update
    Co-authored-by: oljasiforija <olja_siforija@hotmail.com>

--- flask_app/models/recipe.py
+++ flask_app/models/recipe.py
@@ -12,7 +12,7 @@ class Recipe:
         self.title = data['title']
         self.description = data['description']
         self.prep_time = data['prep_time']
-        self.meal_type = data['meal_type']
+        self.meal_type = data['name']
         self.proteins = None
         self.cuisines = None
         # need to add more variables here later
@@ -27,6 +27,22 @@ class Recipe:
         query = f'INSERT INTO recipes_cuisines (recipe_id, cuisine_id) VALUES ({new_recipe_id}, %(cuisine_id)s);'
         connectToMySQL(cls.db).query_db(query, data)
 
+    @classmethod
+    def get_recipes(cls):
+        query = 'SELECT * FROM recipes r JOIN meal_types m ON r.meal_type_id = m.id;'
+        results = connectToMySQL(cls.db).query_db(query)
+        recipes = []
+        for recipe in results:
+            recipes.append(cls(recipe))
+        print(recipes[0].title)
+        return recipes
+    
+    @classmethod
+    def get_single_recipe(cls,data):
+        query = 'SELECT * FROM recipes WHERE id_recipes = %(id_recipes)s'
+        results = connectToMySQL(cls.db).query_db(query, data)
+        recipe = Recipe(results[0])
+        return recipe
 
     @staticmethod
     def recipe_validator(data):
@@ -46,69 +62,3 @@ class Recipe:
         #     flash('please provide a description less than or equal to 500 characters')
         #     is_valid = False
         return is_valid
-
-    @classmethod
-    def get_painting(cls, data):
-        query = 'SELECT * FROM paintings p JOIN users u ON p.user_id = u.id WHERE p.id = %(id)s;'
-        result = connectToMySQL(cls.db).query_db(query,data)
-        query_two = 'SELECT COUNT(painting_id) c FROM purchases WHERE painting_id = %(id)s;'
-        result_two = connectToMySQL(cls.db).query_db(query_two,data)
-
-        new_user = {
-            'id': result[0]['u.id']
-            ,'first_name': result[0]['first_name']
-            ,'last_name': result[0]['last_name']
-            ,'email': result[0]['email']
-            ,'password': None
-            ,'created_at': result[0]['created_at']
-            ,'updated_at': result[0]['updated_at'] 
-        }
-        
-        new_painting = Painting(result[0])
-        new_painting.user = login_reg.User(new_user)
-        new_painting.number_purchased = int(result_two[0]['c'])
-        return new_painting
-
-    @classmethod
-    def add_painting(cls,data):
-        query = 'INSERT INTO paintings (title, description, price, quantity, user_id) VALUES(%(title)s,%(description)s,%(price)s,%(quantity)s,%(user_id)s);'
-        connectToMySQL(cls.db).query_db(query,data)
-
-    @classmethod
-    def delete_painting(cls,data):
-        query = 'DELETE FROM paintings WHERE id = %(id)s;'
-        connectToMySQL(cls.db).query_db(query,data)
-
-    @classmethod
-    def edit_painting(cls, data):
-        query = 'UPDATE paintings SET title = %(title)s, description = %(description)s, price = %(price)s, quantity = %(quantity)s WHERE id = %(id)s;'
-        connectToMySQL(cls.db).query_db(query,data)
-
-    @classmethod
-    def add_purchase(cls, data):
-        query = 'INSERT INTO purchases (user_id, painting_id) VALUES(%(user_id)s,%(painting_id)s);'
-        connectToMySQL(cls.db).query_db(query,data)
-
-    @classmethod
-    def get_user_purchases(cls,data):
-        query = 'SELECT * FROM purchases pp JOIN paintings p ON pp.painting_id = p.id JOIN users uu ON p.user_id = uu.id WHERE pp.user_id = %(id)s;'
-        results = connectToMySQL(cls.db).query_db(query,data)
-
-        purchases = []
-        for painting in results:
-            new_painting = Painting(painting)
-            
-
-            new_user = {
-                'id': painting['id']
-                ,'first_name': painting['first_name']
-                ,'last_name': painting['last_name']
-                ,'email': painting['email']
-                ,'password': None
-                ,'created_at': painting['created_at']
-                ,'updated_at': painting['updated_at'] 
-            }
-
-            new_painting.user = login_reg.User(new_user)
-            purchases.append(new_painting)
-        return purchases
--- flask_app/models/schedule.py
+++ flask_app/models/schedule.py
@@ -4,7 +4,6 @@ from datetime import date, datetime, timedelta, time
 from flask import flash
 
 class Schedule:
-
     def  __init__(self, data) -> None:
         self.id = data['id']
         self.user_id = data['user_id']
@@ -22,21 +21,23 @@ class Schedule:
         return arr[weekday_num]
 
     @classmethod
-    def add_schedule():
-        pass
+    def add_schedule(cls, data):
+        query = "INSERT INTO schedules (date, user_id, prep_time, meal_type_id) VALUES (%(date)s, %(user_id)s, %(prep_time)s, %(meal_type_id)s)"
+        connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
-    def edit_schedule():
-        pass
+    def edit_schedule(cls, data):
+        query = "UPDATE schedules (date = %(date)s, user_id = %(user_id)s, prep_time = %(prep_time)s, meal_type_id = %(meal_type_id)s) WHERE id = %(id)s"
+        connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
     def get_scheduled_recipes(cls, data):
-        query = 'SELECT * FROM schedules s JOIN recipes r ON s.recipe_id = r.id  JOIN meal_types m ON s.meal_type_id = m.id JOIN preptimes pt ON s.preptime_id = pt.id JOIN recipes_cuisines rc ON r.id = rc.recipe_id  JOIN cuisines c ON rc.cuisine_id = c.id JOIN recipes_proteins rp ON r.id = rp.protein_id JOIN proteins p ON rp.protein_id = p.id WHERE s.id = %(user_id)s;'
+        query = 'SELECT * FROM schedules s JOIN recipes_schedules rs ON s.id = rs.schedule_id JOIN recipes r ON s.recipe_id = r.id  JOIN meal_types m ON s.meal_type_id = m.id JOIN preptimes pt ON s.preptime_id = pt.id JOIN recipes_cuisines rc ON r.id = rc.recipe_id  JOIN cuisines c ON rc.cuisine_id = c.id JOIN recipes_proteins rp ON r.id = rp.protein_id JOIN proteins p ON rp.protein_id = p.id WHERE s.id = %(user_id)s;'
         results = connectToMySQL(cls.db).query_db(query, data)
 
         schedule_days = []
         recipes = []
-        proteins = []
+        diets = []
         cuisines = []
 
         for day in results:
@@ -49,7 +50,6 @@ class Schedule:
             
             recipe_data = {
                 'id': None
-
             }
 
             
